e22d8d1d4995fada34ad845b3fc285f3e687f9b3
ashubambal <ashutoshbambal97@gmail.com>
2026-03-08 09:49:09 +0530

    upf

--- terraform-repo/prod/ap-northeast-2/shared_eks/01/eks/main.tf
+++ terraform-repo/prod/ap-northeast-2/shared_eks/01/eks/main.tf
@@ -16,36 +16,32 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "my-terraform-state-ashu-abc-xyz"
-    key    = "prod-vpc.tfstate"
+    key    = "prod-vpc.tfstate"   # must match the VPC backend key
     region = "ap-northeast-2"
   }
 }
 
+data "terraform_remote_state" "bastion" {
+  backend = "s3"
+  config = {
+    bucket = "my-terraform-state-ashu-abc-xyz"
+    key    = "prod-bastion.tfstate"   # if you have a bastion module separately
+    region = "ap-northeast-2"
+  }
+}
 
 module "eks" {
   source                  = "git::https://github.com/soft-consist/terraform-modules.git//modules/eks?ref=v9.0.27"
   env                     = var.env
   cluster_name            = var.cluster_name
   cluster_version         = var.cluster_version
-  vpc_id                  = module.vpc.aws_vpc
-  private_subnet_ids      = module.vpc.private_subnet_ids
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.aws_vpc
+  private_subnet_ids      = data.terraform_remote_state.vpc.outputs.private_subnet_ids  
   tags                    = var.tags
   desired_size            = var.desired_size
   max_size                = var.max_size
   min_size                = var.min_size
   node_instance_types     = var.node_instance_types
   allowd_cidr_blocks      = var.allowd_cidr_blocks
-  bastion_access_role_arn = module.bastion.bastion_access_role_arn
+  bastion_access_role_arn = data.terraform_remote_state.bastion.outputs.bastion_access_role_arn
 }
-
-output "aws_vpc" {
-  value = module.vpc.aws_vpc
-}
-
-output "public_subnet_ids" {
-   value = module.vpc.public_subnet_ids
- }
-
-output "bastion_access_role_arn" {
-   value = module.bastion.bastion_access_role_arn
- }
