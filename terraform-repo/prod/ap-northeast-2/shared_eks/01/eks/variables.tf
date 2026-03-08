9bc83dce7c91e17552bd9545333061e36157b306
ashubambal <ashutoshbambal97@gmail.com>
2026-03-08 09:29:34 +0530

    upf

--- terraform-repo/prod/ap-northeast-2/shared_eks/01/eks/variables.tf
+++ terraform-repo/prod/ap-northeast-2/shared_eks/01/eks/variables.tf
@@ -1,61 +1,61 @@
-# variable "cluster_name" {
-#   description = "EKS Cluster Name"
-#   type        = string
-# }
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+}
 
-# variable "cluster_version" {
-#   description = "EKS Cluster Version"
-#   type        = string
-# }
+variable "cluster_version" {
+  description = "EKS Cluster Version"
+  type        = string
+}
 
-# variable "vpc_id" {
-#   description = "VPC ID"
-#   type        = string
-#   default     = ""
-# }
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+  default     = ""
+}
 
-# variable "private_subnet_ids" {
-#   description = "Private Subnets IDs"
-#   type        = list(string)
-#   default     = []
-# }
+variable "private_subnet_ids" {
+  description = "Private Subnets IDs"
+  type        = list(string)
+  default     = []
+}
 
-# variable "desired_size" {
-#   description = "Desired size of the node group"
-#   type        = number
-#   default     = 2
-# }
+variable "desired_size" {
+  description = "Desired size of the node group"
+  type        = number
+  default     = 2
+}
 
-# variable "max_size" {
-#   description = "Maximum size of the node group"
-#   type        = number
-#   default     = 4
-# }
+variable "max_size" {
+  description = "Maximum size of the node group"
+  type        = number
+  default     = 4
+}
 
-# variable "min_size" {
-#   description = "Minimum size of the node group"
-#   type        = number
-#   default     = 1
-# }
+variable "min_size" {
+  description = "Minimum size of the node group"
+  type        = number
+  default     = 1
+}
 
-# variable "node_instance_types" {
-#   description = "Instance types for the node group"
-#   type        = list(string)
-# }
+variable "node_instance_types" {
+  description = "Instance types for the node group"
+  type        = list(string)
+}
 
-# variable "allowd_cidr_blocks" {
-#   description = "Allowed CIDR blocks for access"
-#   type        = list(string)
-#   default     = []
-# }
+variable "allowd_cidr_blocks" {
+  description = "Allowed CIDR blocks for access"
+  type        = list(string)
+  default     = []
+}
 
-# variable "public_subnet_ids" {
-#   description = "Public Subnets IDs"
-#   type        = list(string)
-#   default     = []
-# }
+variable "public_subnet_ids" {
+  description = "Public Subnets IDs"
+  type        = list(string)
+  default     = []
+}
 
-# variable "region" {
-#   description = "AWS region"
-#   type        = string
-# }
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
